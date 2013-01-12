eb7dd2d640b600082fbba519f76ba79f48333bf2
Jon Craton <jncraton@gmail.com>
2013-01-11 21:20:49 -0500

    added commit hash message at start of game

--- src/build_ai.js
+++ src/build_ai.js
@@ -1,10 +1,17 @@
 var fs = require('fs');
 var AI = require('./ai');
+var exec = require('child_process').exec;
 
 var src;
 if(process.argv[3]) {
     src = AI(process.argv[2]).build();
+    
+exec('git rev-parse HEAD', function (error, commit) {
+    commit = commit.replace('\n', '');
+    src = src.replace(/{commit}/g, commit);
     fs.writeFileSync(process.argv[3], src);
+});
+    
 } else {
     console.log('Usage: node ' + process.argv[1] + ' race output');
 }
