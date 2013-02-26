ebc41fd26332fea1e56d269ecce6a928683eb46e
Jon Craton <jncraton@gmail.com>
2013-02-26 12:14:22 -0500

    now works with units

--- tools/buildConverter.js
+++ tools/buildConverter.js
@@ -1,16 +1,47 @@
+var units = require('./units.json');
+var abbrevs = require('./abbrevs.js');
+
+function isUnit(unit) {
+    return units.some(function (u) {
+        return u.unit === unit;
+    });
+}
+
+function getUnitInfo(unit) {
+    return units.filter(function (u) {
+        return u.unit === unit;
+    })[0];
+}
+
 var parse = function parse(content) {
     var owned = {};
+    var supplyFromUnits = 0;
     
-    content = content.replace(/^(\d+) (.*)$/mg, function(original, supply, building) {
-        if(!owned[building]) {
-            owned[building] = 0;
-        }
-        owned[building] += 1;
+    content = content.replace(/^(\d+) (.*)$/mg, function(original, supply, unit) {
+        var ret = "";
         
-        return 'build(' + supply + ', Peon, 80)\n' +
-                  'wait_buildstart(' + supply + ', Peon)\n' +
-                  'build(' + owned[building] + ', ' + building + ', 80)\n' +
-                  'wait_buildstart(' + owned[building] + ', ' + building + ')\n';
+        unit = abbrevs.expand(unit);
+        
+        if(!owned[unit]) {
+            owned[unit] = 0;
+        }
+        owned[unit] += 1;
+
+        var waitForWorker = supply - supplyFromUnits;
+        
+        ret += 'build(' + waitForWorker + ', Peon, 80)\n' +
+               'wait_buildstart(' + waitForWorker + ', Peon)\n';
+        
+        if (isUnit(unit)) {
+            supplyFromUnits += parseInt(getUnitInfo(unit).supply);
+            
+            ret += 'train(' + owned[unit] + ', ' + unit + ')\n';
+        } else {
+            ret += 'build(' + owned[unit] + ', ' + unit + ', 80)\n' +
+                   'wait_buildstart(' + owned[unit] + ', ' + unit + ')\n';
+        }
+        
+        return ret;
     });
     
     return content;
