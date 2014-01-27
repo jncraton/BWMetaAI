a6afb95c56f9b4732484e6ae33a2c4d3feef9bb5
Jon Craton <jncraton@gmail.com>
2014-01-26 19:44:28 -0500

    removed choice delay

--- tools/race.js
+++ tools/race.js
@@ -91,7 +91,6 @@ function Race(name) {
             
             append('--gen_' + dir + '--')
             append('--gen_jump_loop' + dir + '--')
-            append('wait(50)')
             
             var files = [];
                 
