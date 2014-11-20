f1d56756538d9958dca488931c09c6ebb595f0f1
Jon Craton <jncraton@gmail.com>
2014-11-20 13:27:12 -0500

    added tools to build package for BWAILauncher #10

--- tools/build_ai.js
+++ tools/build_ai.js
@@ -1,17 +1,20 @@
 var fs = require('fs');
-var AI = require('./ai');
-var exec = require('child_process').exec;
 
-var src;
+exports.build = function(input, output) {
+    var AI = require('./ai');
+    var exec = require('child_process').exec;
+
+    var src = AI(input).build();
+    
+    exec('git rev-parse HEAD', function (error, commit) {
+        commit = commit.replace('\n', '').substring(0,6);
+        src = src.replace(/{commit}/g, commit);
+        fs.writeFileSync(output, src);
+    });
+}
+
 if(process.argv[3]) {
-    src = AI(process.argv[2]).build();
-    
-exec('git rev-parse HEAD', function (error, commit) {
-    commit = commit.replace('\n', '').substring(0,6);
-    src = src.replace(/{commit}/g, commit);
-    fs.writeFileSync(process.argv[3], src);
-});
-    
+    exports.build(process.argv[2], process.argv[3])
 } else {
     console.log('Usage: node ' + process.argv[1] + ' race output');
 }
